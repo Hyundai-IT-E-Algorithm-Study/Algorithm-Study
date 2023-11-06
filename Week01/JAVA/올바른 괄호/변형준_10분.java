a19c4f56a4d8681602782e1bb0e580a7cdc31bf5
skybluelion <brotherjun96@gmail.com>
2023-11-06 18:28:33 +0900

    Update 변형준_10분.java

--- Week01/JAVA/올바른 괄호/변형준_10분.java
+++ Week01/JAVA/올바른 괄호/변형준_10분.java
@@ -1,7 +1,8 @@
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
         int cnt = 0;
+        //괄호가 ")" 가 많으면 불가능 
+        //cnt로 for문 중 음수 혹은 끝나고 0이 안되면 불가능
         for(int i = 0; i < s.length(); i++){
             String str = String.valueOf(s.charAt(i));
             if(cnt < 0){
@@ -14,6 +15,6 @@ class Solution {
             }
         }
         if(cnt != 0) return false;
-        return answer;
+        else return true;
     }
 }
