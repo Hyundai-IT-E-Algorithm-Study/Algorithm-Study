117099a28dab32513c015758ef71b93c02031603
roonhyeon <128015166+roonhyeon@users.noreply.github.com>
2023-11-14 23:19:54 +0900

    Create 황수연_30분.sql

---
+++ Week02/SQL/조건별로 분류하여 주문상태 출력하기/황수연_30분.sql
@@ -0,0 +1,7 @@
+SELECT order_id, product_id, to_char(out_date,'YYYY-MM-DD') as out_date,
+       case when to_char(out_date,'YYYY-MM-DD') <= '2022-05-01' then '출고완료'
+            when to_char(out_date,'YYYY-MM-DD') > '2022-05-01' then '출고대기'
+            else '출고미정'
+            end as 출고여부
+from food_order
+order by order_id asc;
