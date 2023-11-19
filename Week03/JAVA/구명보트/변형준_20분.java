baa0a6f4213d09ac465c0830b41eca39aeeff28b
skybluelion <brotherjun96@gmail.com>
2023-11-19 16:22:54 +0900

    Update 변형준_20분.java

--- Week03/JAVA/구명보트/변형준_20분.java
+++ Week03/JAVA/구명보트/변형준_20분.java
@@ -1,3 +1,4 @@
+//풀이1
 import java.util.*;
 
 // 정확성 81.5 효율성 18.5
@@ -35,3 +36,40 @@ class Solution {
         return answer;
     }
 }
+
+//풀이2
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        //정렬
+        Arrays.sort(people);
+        int[] sortedPeople = new int[people.length];
+        int idx = 0;
+        for(int i = people.length - 1; i >= 0; i--){
+            sortedPeople[idx++] = people[i];
+        }
+        
+        //
+        int endIdx = people.length - 1;
+        int currWeight, endWeight, sum = 0;
+        
+        for(int currIdx = 0; currIdx < people.length; currIdx++){
+            currWeight = sortedPeople[currIdx];
+            endWeight = sortedPeople[endIdx];
+            sum = currWeight + endWeight;
+
+            if(endIdx - currIdx < 0) break;
+            else{
+                answer++;
+                if(limit - currWeight < 40) continue;
+                    if(limit >= sum){
+                        endIdx--;
+                    }
+            }
+        }
+        return answer;
+    }
+}
+
