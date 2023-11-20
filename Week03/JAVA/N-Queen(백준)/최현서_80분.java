8615e1fbbcbb455fa88a9dbbebe4bce4fb9b813e
최현서 <117807082+ha01032@users.noreply.github.com>
2023-11-21 00:28:34 +0900

    Update 최현서_80분.java

--- Week03/JAVA/N-Queen(백준)/최현서_80분.java
+++ Week03/JAVA/N-Queen(백준)/최현서_80분.java
@@ -5,45 +5,54 @@ import java.util.Scanner;
 public class Main {
 	static int n;
 	static int answer;
-	
+
 	static int[] queens = null;
 	static int queenY;
+	static boolean isPossible;
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 		answer = 0;
-		queens = new int[n+1]; //index를 x좌표, 값을 y좌표로 하는 배열 초기화(1~n까지)
-		
+		queens = new int[n + 1]; // index를 x좌표, 값을 y좌표로 하는 n 길이 배열 생성
+
 		makeQueens(1);
 		System.out.println(answer);
 
 	}
-	
-	//x값이 같으면 x축방향 공격이 되므로, x=1부터 n까지 깊이탐색(재귀) 수행->"x축공격 가지치기"
+
+	// x값이 같으면 x축방향 공격이 되므로, x=1부터 n까지 깊이 탐색 수행->"x축방향 공격 가지치기"
 	static void makeQueens(int x) {
-		if (x > n) { //가지치기 안당하고 n까지 도달해야 n개의 퀸을 다 채운 것
+		if (x > n) { // 가지치기 안당하고 n까지 도달해야 n개의 퀸을 다 채운 것
 			answer++;
 			return;
-    }
+		}
+		
 		for (int y = 1; y <= n; y++) {
-			if (isPossible(x,y)) {//"나머지 공격 가지치기" 수행함수(밑에 있음)
-				queens[x] = y; // 해당 위치에 queen 놔둠
-				makeQueens(x + 1);
+			
+			isPossible = true;
+			//가지치기 for문
+			for (int i = 1; i < x; i++) {//이전에 놓아진 x-1개의 퀸들을 배열에서 하나씩 빼서 공격가능여부 확인
+				queenY = queens[i]; // y값 할당
+				if (y == queenY) {
+					isPossible=false;
+					break;} // y값이 같으면 y축 방향으로 공격 받음
+				else if (x + y == i + queenY) {
+					isPossible=false;
+					break;} // x+y가 같으면 5시 대각선 방향으로 공격 받음
+				else if (x - y == i - queenY) {
+					isPossible=false;
+					break;} // x-y가 같으면 7시 대각선 방향으로 공격 받음
 			}
+			
+			
+			// 가지치기 당하지 않은 x,y 배열에 넣고, 다음 재귀 수행
+			if(isPossible) {
+            queens[x] = y;
+			makeQueens(x + 1);
+            }
+
 		}
-	}
-	
-	
-	
-	static boolean isPossible(int x, int y) {
-		for (int i=1; i<x;i++) {//i는 배열의 x값과 같다.(매개변수x 이전값까지 할당됐으므로 i<x)
-			queenY = queens[i]; //배열값의 y값 
-			if (y==queenY) return false; //y값이 같으면 y축 방향으로 공격 받음
-			else if (x+y == i+queenY) return false; // x+y가 같으면 5시 대각선 방향으로 공격 받음
-			else if (x-y == i-queenY) return false; // x-y가 같으면 7시 대각선 방향으로 공격 받음
-		}
-		return true; //3가지 경우 아니면 퀸을 놓아도 되는 위치임
 	}
 }
