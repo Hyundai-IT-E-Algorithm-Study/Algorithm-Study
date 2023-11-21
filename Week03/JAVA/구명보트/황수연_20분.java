ac9b846718a4ae0de106539ca76117c39ec5c171
roonhyeon <128015166+roonhyeon@users.noreply.github.com>
2023-11-21 09:24:24 +0900

    Update 황수연_20분.java

--- Week03/JAVA/구명보트/황수연_20분.java
+++ Week03/JAVA/구명보트/황수연_20분.java
@@ -7,7 +7,7 @@ class Solution {
         int min_weight = 0; // 최소 몸무게
         
         for(int i = people.length-1; i >= min_weight; i--) { // 최대 몸무게부터 내림차순
-            if(people[i] + people[min_weight] <= limit) { // 최대 몸무게 + 최소 몸무게 < 구명보트 무게 제한
+            if(people[i] + people[min_weight] <= limit) { // 최대 몸무게 + 최소 몸무게 <= 구명보트 무게 제한
                 min_weight++; // 최소 무게 갱신
                 answer++;
             } else { // 최대 몸무게 한명만 태우기(최소 몸무게 그대로 남아있음)
