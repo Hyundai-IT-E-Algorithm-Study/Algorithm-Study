68f3df42dbfc6d342756cd432d61ddd9d3d33dd7
BanJung <118974992+BanJung@users.noreply.github.com>
2023-11-22 19:31:45 +0900

    Create 반정현.java

---
+++ Week03/JAVA/구명보트/반정현.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int start=0;
+        int end=people.length-1;
+        while (end>=start) {
+        	if(people[start]+people[end]<=limit) start++;
+        	end--;
+        	answer++;    	
+        }
+        return answer;
+    }
+}
