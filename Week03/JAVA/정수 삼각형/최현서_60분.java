b11c39b35464b6790826a9cab78dc03d7ed3120e
최현서 <117807082+ha01032@users.noreply.github.com>
2023-11-23 03:58:10 +0900

    Update 최현서_60분.java

--- Week03/JAVA/정수 삼각형/최현서_60분.java
+++ Week03/JAVA/정수 삼각형/최현서_60분.java
@@ -3,18 +3,18 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[][] triangle) {
     	//원래 DP배열을 따로 만들어주는게 정석이지만,
-		//이 문제에서는 triangle배열을 그대로 DP로 써도 정답을 맞추는데 영향을 안줘서
-		//triangle을 DP로 썼음
+	//이 문제에서는 triangle배열을 그대로 DP로 써도 정답을 맞추는데 영향을 안줘서
+	//triangle을 DP로 썼음
     	for (int row=1; row<triangle.length; row++) {
     		for(int i=0; i<row+1; i++) {
     			if (i==0) 
-					//맨 왼쪽일땐, 오른쪽 위의 DP값에 현재 값 더해준게 최대 합계
+				//맨 왼쪽일땐, 오른쪽 위의 DP값에 현재 값 더해준게 최대 합계
     				triangle[row][i] = triangle[row][i] + triangle[row-1][i];
     			else if (i==row)
-					//맨 오른쪽일땐, 왼쪽 위의 DP값에 현재 값 더해준게 최대 합계
+				//맨 오른쪽일땐, 왼쪽 위의 DP값에 현재 값 더해준게 최대 합계
     				triangle[row][i] = triangle[row][i] + triangle[row-1][i-1];
     			else
-					//사이에 있을 땐, 왼쪽 위 DP값과 오른쪽 위 DP값 중 큰거에 현재 값 더해준게 최대 합계
+				//사이에 있을 땐, 왼쪽 위 DP값과 오른쪽 위 DP값 중 큰거에 현재 값 더해준게 최대 합계
     				triangle[row][i] = triangle[row][i] + Math.max(triangle[row-1][i], triangle[row-1][i-1]);
     		}
     	}
