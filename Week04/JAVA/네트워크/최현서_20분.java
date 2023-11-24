11d620e616cfb7d759f1484384c76e0738aae97c
최현서 <117807082+ha01032@users.noreply.github.com>
2023-11-24 22:17:58 +0900

    Update 최현서_20분.java

--- Week04/JAVA/네트워크/최현서_20분.java
+++ Week04/JAVA/네트워크/최현서_20분.java
@@ -10,7 +10,7 @@ class Solution {
 
       //방문하지 않은 컴퓨터를 root로 dfs수행:
       //dfs나 bfs가 수행이 됐다는 것 == 서로 연결돼있다
-      //dfs(bfs)수행개수 == 네트워크수
+      //dfs(bfs)수행 횟수 == 네트워크수
      
     	for (int computer=0; computer<n; computer++) {
     		if (visited[computer]) continue;
