3696d31290116431c9e8af1774a84c267816ab15
최현서 <117807082+ha01032@users.noreply.github.com>
2023-11-30 15:14:18 +0900

    Update 최현서_30분.java

--- Week05/JAVA/카펫/최현서_30분.java
+++ Week05/JAVA/카펫/최현서_30분.java
@@ -3,9 +3,9 @@ class Solution {
         int[] answer = {0,0};
         //전체 영역의 넓이(칸수)는 두 색의 칸수 합과 같다.
         int area = brown + yellow;
-        // 가로길이는 3(yellow가 1이상이므로)~area범위에서 벗어날 수 없으므로
+        // 가로(세로)길이는 3(yellow가 1이상이므로)~area범위에서 벗어날 수 없으므로
         // 이 범위에서 나머지 조건 완전 탐색
-        for(int w=3; w<=area; w++){
+        for(int w=3; w<area; w++){
             double h=(double)area/w; //세로길이는 넓이에서 가로길이로나눈 것
             if(w >= h){
               if((w-2)*(h-2) == yellow){ //가로,세로에 2씩 빼주고 곱한게 yellow와 같다
