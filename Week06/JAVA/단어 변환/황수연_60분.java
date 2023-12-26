5161ddc4be6a888c9ea52f40d0d9d0553b11ed60
roonhyeon <128015166+roonhyeon@users.noreply.github.com>
2023-12-26 19:06:35 +0900

    Update 황수연_60분.java

--- Week06/JAVA/단어 변환/황수연_60분.java
+++ Week06/JAVA/단어 변환/황수연_60분.java
@@ -18,7 +18,7 @@ class Solution {
 		for (int i = 0; i < words.length; i++) {
 			if (visited[i]) continue; // 이미 방문한 words면 통과
             
-      // 겹치는 철자 세기
+      			// 겹치는 철자 세기
 			int same = 0;
 			for (int j = 0; j < begin.length(); j++) {
 				if (begin.charAt(j) == words[i].charAt(j)) { 
@@ -29,7 +29,7 @@ class Solution {
 			if (same == begin.length() - 1) { // 한 글자 빼고 모두 같은 경우(hit, hot 같은 경우)
 				visited[i] = true;
 				dfs(words[i], target, words, cnt + 1); // 재귀 for 다음 변환
-        visited[i] = false; // 다음 변환을 위해 방문 초기화 해주기(아이디어 찾아봄)
+        			visited[i] = false; // 다음 변환을 위해 방문 초기화 해주기(아이디어 찾아봄)
 			}
 		}
 	}
