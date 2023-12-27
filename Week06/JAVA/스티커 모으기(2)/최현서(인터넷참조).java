46e6654b0f248e0a635f04179c20c670702b05e5
최현서 <117807082+ha01032@users.noreply.github.com>
2023-12-27 15:29:29 +0900

    Update 최현서(인터넷참조).java

--- Week06/JAVA/스티커 모으기(2)/최현서(인터넷참조).java
+++ Week06/JAVA/스티커 모으기(2)/최현서(인터넷참조).java
@@ -4,33 +4,27 @@ class Solution {
         
         //원형이라, dp 2개 사용
         //맨처음거 포함하는 dp1
-        int[] dp1 = new int[stickerNum];
+        int[] dp1 = new int[stickerNum<=2?2:stickerNum-1];
         //맨처음거 포함 안하는 dp2
         int[] dp2 = new int[stickerNum];
 
-        int dp1max = 0;
-        int dp2max = 0;
 
         for (int i=0; i< stickerNum; i++){
             if (i==0){
-                dp1[i] = sticker[i];
-                dp1max = dp1[i];
+                dp1[0] = sticker[i];
+                dp1[1] = dp1[0];
             }
             if (i==1){
-                dp1[i] = dp1[0];
                 dp2[i] = sticker[i];
-                dp2max = dp2[i];
             }
 
             if (1<i) {
                 dp2[i]= Math.max(dp2[i-2]+sticker[i], dp2[i-1]);
-                dp2max = dp2[i];
                 if(i<=stickerNum-2){
                     dp1[i] = Math.max(dp1[i-2]+sticker[i], dp1[i-1]);
-                    dp1max = dp1[i];
                 }
             }
         }
-        return Math.max(dp1max, dp2max);
+        return Math.max(dp1[dp1.length-1], dp2[dp2.length-1]);
     }
 }
